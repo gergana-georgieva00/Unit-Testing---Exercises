f024408232203b0bde3ebdbc8b58df502bb37038
Gergana Georgieva <gerganagergana1@gmail.com>
2022-11-26 14:21:25 +0200

    Unit Testing - Exercises

--- Database.Tests/DatabaseTests.cs
+++ Database.Tests/DatabaseTests.cs
@@ -1,10 +1,24 @@
 namespace Database.Tests
 {
     using NUnit.Framework;
+    using System;
 
     [TestFixture]
     public class DatabaseTests
     {
+        //[Test]
+        //public void ArrayCapcityMustBe16IntegersLongOtherwiseThrow()
+        //{
+        //    Database db = new Database(new int[20]);
+        //    Assert.Throws<InvalidOperationException>(() => db.Count.Equals(16));
+        //    Assert.That(db.Count, Is.EqualTo(16), );
+        //}
 
+        [Test]
+        public void AddMethodWithFullArrayShouldThrow()
+        {
+            Database db = new Database(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 });
+            Assert.Throws<InvalidOperationException>(() => db.Add(1));
+        }
     }
 }
