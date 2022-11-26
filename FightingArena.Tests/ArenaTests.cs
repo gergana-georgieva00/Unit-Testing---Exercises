7a3198c0ecae215c753fc7247f61efe8d6037509
Gergana Georgieva <gerganagergana1@gmail.com>
2022-11-27 00:58:40 +0200

    Unit Testing - Exercises

--- FightingArena.Tests/ArenaTests.cs
+++ FightingArena.Tests/ArenaTests.cs
@@ -5,6 +5,18 @@
     [TestFixture]
     public class ArenaTests
     {
+        private Arena arena;
 
+        [SetUp]
+        public void SetUp()
+        {
+            arena = new Arena();
+        }
+
+        [Test]
+        public void WarriorsGetterShouldWorkCorrectly()
+        {
+            Assert.That(arena.Warriors, Is.EqualTo(new Arena().Warriors));
+        }
     }
 }
