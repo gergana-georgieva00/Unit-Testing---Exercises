148acb9cc3f17618047db5c2e6eea9f42c591ae1
Gergana Georgieva <gerganagergana1@gmail.com>
2022-11-26 22:16:29 +0200

    Unit Testing - Exercises

--- CarManager.Tests/CarManagerTests.cs
+++ CarManager.Tests/CarManagerTests.cs
@@ -1,10 +1,23 @@
 namespace CarManager.Tests
 {
     using NUnit.Framework;
+    using System;
 
     [TestFixture]
     public class CarManagerTests
     {
+        private Car car;
 
+        [SetUp]
+        public void Setup()
+        {
+            car = new Car("make", "model", 10, 20);
+        }
+
+        [Test]
+        public void MakeCannotBeNullOrEmpty()
+        {
+            Assert.Throws<ArgumentException>(() => car = new Car("", "model", 10, 20));
+        }
     }
 }
